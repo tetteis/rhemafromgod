10f07a7a0245265b21a27083d7417ecf4656d06b
Tettei Shahday-Annang <tetteis@gmail.com>
2022-06-01 10:59:31 +0000

    Initial commit

---
+++ components/comments/index.js
@@ -0,0 +1,23 @@
+import siteMetadata from '@/data/siteMetadata'
+import dynamic from 'next/dynamic'
+
+const DisqusComponent = dynamic(
+  () => {
+    return import('@/components/comments/Disqus')
+  },
+  { ssr: false }
+)
+
+const Comments = ({ frontMatter }) => {
+  const comment = siteMetadata?.comment
+  if (!comment || Object.keys(comment).length === 0) return <></>
+  return (
+    <div id="comment">
+      {siteMetadata.comment && siteMetadata.comment.provider === 'disqus' && (
+        <DisqusComponent frontMatter={frontMatter} />
+      )}
+    </div>
+  )
+}
+
+export default Comments
