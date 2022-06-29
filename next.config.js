3fb42fb07169ec6a2240a84b548a18e7d1ede817
Tettei Shahday-Annang <tetteis@gmail.com>
2022-06-29 10:28:22 +0000

    Updated next config

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://rhemafromgod.disqus.com https://www.google-analytics.com https://www.googletagmanager.com https://api.reftagger.com https://reftagger.bibliacdn.com;
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.disqus.com a.disquscdn.com https://www.google-analytics.com https://www.googletagmanager.com https://api.reftagger.com https://reftagger.bibliacdn.com;
+  style-src 'self' 'unsafe-inline' a.disquscdn.com;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src https://rhemafromgod.disqus.com;
+  frame-src disqus.com;
 `
 
 const securityHeaders = [
